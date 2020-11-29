163faac038b740c2f0794f47c4d732ce3ad538c2
Evgeniy Chernyshenko <magdegaben@gmail.com>
2020-11-29 12:05:41 +0200

    adding structure and base markup

---
+++ src/JS/refs.js
@@ -0,0 +1,10 @@
+export default {
+    header: document.querySelector('.headerJs')
+    nav: document.querySelector('.navJs'),
+    ul: document.querySelector('.navList'),
+
+    homeSec = document.querySelector('.home'),
+    aboutSec = document.querySelector('.about'),
+    contactSec = document.querySelector('.contacts')
+
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import css from "./css/styles.css";
 
-class User {
-  constructor(name) {
-  this._name = name
-}
-  get name() {
-   return this._name
- }
-}
-const mango = new User('MANGO')
-console.log(mango.name);
+import "./JS/home.js";
+import "./JS/about.js";
+import "./JS/contacts.js";
